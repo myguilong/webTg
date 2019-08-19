125cb3cb3288670fc93ed033794afaad304ec47a
myguilong <1360747193@qq.com>
2019-08-19 13:58:39 +0800

    提交团长申请

--- src/http.js
+++ src/http.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
 const http = axios.create({
-    baseURL:'http://localhost/apis'
+    baseURL:'http://localhost/apis',
+    withCredentials: true 
 })
 export default http
--- src/views/headerCreate.vue
+++ src/views/headerCreate.vue
@@ -14,14 +14,13 @@
       <van-field v-model="phone" type="text" label="手机号" placeholder="请输入手机号" required />
       <!-- <van-field v-model="city" type="text" label="城市" placeholder="请输入城市" required /> -->
 
-      <van-cell icon="location-o" title="位置">
-         {{address}}
+      <van-cell icon="location-o" :title="address">
         <van-button type="primary" size="small" @click="geo">获取当前位置</van-button>
       </van-cell>
-      <van-field v-model="password" type="text" label="详细地址" placeholder="请输入详细地址" required />
+      <van-field v-model="unAddress" type="text" label="详细地址" placeholder="请输入详细地址" required />
       <van-uploader v-model="fileList" :after-read="afterRead" multiple :max-count="1" />
       <van-cell title="上传头像"></van-cell>
-      <van-button type="primary" size="large">申请团长</van-button>
+      <van-button type="primary" size="large" @click="toCreate">申请团长</van-button>
     </van-cell-group>
   </div>
 </template>
@@ -36,17 +35,29 @@ export default {
       headerName: "",
       phone: "",
       city: "",
-      password: "",
       imgstr: "",
       address: "",
       longtitude: "",
-      latitude: ""
+      latitude: "",
+      unAddress:''
     };
   },
   created(){
         MapLoader();
   },
   methods: {
+    async toCreate(){
+           let res = await this.$http.post('/header/create',{
+             longitude:this.longtitude,
+             latitude:this.latitude,
+             address:this.address,
+             city:this.city,
+             unaddress:this.unAddress,
+             imgSrc:this.imgstr,
+             phone:this.phone,
+             name:this.headerName
+           })
+    },
     async afterRead(file) {
       const clinet = new OSS({
         region: "oss-cn-shenzhen",
@@ -67,9 +78,6 @@ export default {
       getPosition()
         .then(result => {
           console.log(result);
-          // 返回结果示例：
-          // {latitude: 30.318030999999998, longitude: 120.05561639999999}
-          // 一般小数点后只取六位，所以用以下代码搞定
           let queryData = {
             longtitude: String(result.longitude).match(/\d+\.\d{0,6}/)[0],
             latitude: String(result.latitude).match(/\d+\.\d{0,6}/)[0]
@@ -78,13 +86,11 @@ export default {
           this.latitude = queryData.latitude;
           console.log(queryData);
          this.$http.get(`https://restapi.amap.com/v3/geocode/regeo?output=json&location=${queryData.longtitude},${queryData.latitude}&key=87699791f29eda4e402ac9e471a4376a&radius=1000&extensions=all`).then(res=>{
-
              console.log(res.data.regeocode.formatted_address)
+             console.log(res,'地址')
+             this.city = res.data.regeocode.addressComponent.city
              this.address = res.data.regeocode.formatted_address
          })
-          // 以下放置获取坐标后你要执行的代码:
-          // ...
-    
         })
         .catch(err => {
           console.log(err);
